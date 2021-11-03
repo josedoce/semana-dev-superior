f5ff8b50fecddebffc74ae7c1b300920f50e3ba8
JAS <66434808+josedoce@users.noreply.github.com>
2021-11-03 13:47:05 -0300

    project created

---
+++ backend/src/main/java/com/sds/bootcamp/app/SdsApplication.java
@@ -0,0 +1,13 @@
+package com.sds.bootcamp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sds/bootcamp/app/SdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.bootcamp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
