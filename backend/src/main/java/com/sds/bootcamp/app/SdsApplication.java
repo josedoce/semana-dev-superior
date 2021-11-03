c8c6a73b0676ff8268704e79ab07c18c13bcb776
JAS <66434808+josedoce@users.noreply.github.com>
2021-11-03 20:58:48 -0300

    Security config

--- backend/src/main/java/com/sds/bootcamp/app/SdsApplication.java
+++ backend/src/main/java/com/sds/bootcamp/app/SdsApplication.java
@@ -2,12 +2,19 @@ package com.sds.bootcamp.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SdsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SdsApplication.class, args);
 	}
-
+	
+	@GetMapping("/teste")
+	public String teste() {
+		return "funcionando";
+	}
 }
